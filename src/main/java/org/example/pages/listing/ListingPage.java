610acbd680311368b7240103214df69bd270bb4e
raketavlad <zerosworld@mail.ru>
2026-09-09 19:02:28 +0300

    Add ListingPage POM

---
+++ src/main/java/org/example/pages/listing/ListingPage.java
@@ -0,0 +1,33 @@
+package org.example.pages.listing;
+
+import org.example.pages.base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+public class ListingPage extends BasePage {
+
+    private final By headline = By.cssSelector("h1.title__text");
+    private final By itemCounter = By.cssSelector("div.products-counter");
+    private final By cards = By.xpath("div.setout__box");
+
+
+    public ListingPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public ListingPage checkHeadline() {
+        Assert.assertEquals(driver.findElement(headline).getText(), "Фиксированные коммутаторы");
+        return this;
+    }
+
+    public ListingPage checkListingItemCount() {
+        Assert.assertEquals(driver.findElement(itemCounter).getText(), 1186);
+        return this;
+    }
+
+    public ListingPage checkPageItemCount() {
+        Assert.assertEquals(driver.findElements(cards).size(), 40);
+        return this;
+    }
+}
